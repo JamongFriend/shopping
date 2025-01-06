158c891185e4ced0a2466ed39479316d4a204e40
JamogFrinds <ghrb6769@gmail.com>
2025-01-06 16:42:05 +0900

    feat: memberService 테스트 코드 작성

--- src/test/java/project/shopping/service/MemberServiceTest.java
+++ src/test/java/project/shopping/service/MemberServiceTest.java
@@ -1,9 +1,12 @@
 package project.shopping.service;
 
+import jakarta.persistence.EntityManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import project.shopping.domain.Member;
@@ -15,12 +18,12 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @Transactional
 public class MemberServiceTest {
-    @Autowired
-    MemberService memberService;
-    @Autowired
-    MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
+    @Rollback(false)
     public void join() throws Exception {
         //given
         Member member = new Member();
@@ -28,8 +31,45 @@ public class MemberServiceTest {
         //when
         Long saveId = memberService.join(member);
         //then
+        em.flush();
         assertEquals(member, memberRepository.findOne(saveId));
 
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void validateMemberCheck() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        memberService.join(member2);
+        fail("예외발생");
+
+    }
+
+    /*
+    이코드가 위의 코드랑 동일함
+    @Test
+    public void validateMemberCheck() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        }
+        catch(IllegalStateException e) {
+            return;
+        }
+        fail("예외발생");
+
+    }
+    */
 
 }
