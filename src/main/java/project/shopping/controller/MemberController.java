3ca51ee086e57dd3364bdfb175e533fc9d0f843d
JamogFrinds <ghrb6769@gmail.com>
2025-02-04 14:47:44 +0900

    feat: 회원 목록 기능, 상품 등록 기능 웹 구현

--- src/main/java/project/shopping/controller/MemberController.java
+++ src/main/java/project/shopping/controller/MemberController.java
@@ -40,10 +40,10 @@ public class MemberController {
 
     }
 
-//    @GetMapping(value = "/members")
-//    public String list(Model model){
-//        List<Member> members = memberService.findMembers();
-//        model.addAttribute("members", members);
-//        return "members/memberList";
-//    }
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
